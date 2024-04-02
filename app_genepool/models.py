0d2cf4240725e17ef2b9a3ca4cde24ad996d36af
Blaize Egelhof <Blaize.Egelhof@outlook.com>
2024-04-02 10:47:35 +0000

    heroku models push

--- app_genepool/models.py
+++ app_genepool/models.py
@@ -27,7 +27,7 @@ class UnauthorisedQuoteRequests(models.Model):
     time_requested = models.DateTimeField(default=timezone.now)
     closed_by = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-        related_name='Closed_requestsQuote', verbose_name='Closedquote', blank=True)
+        related_name='Closed_requestsQuote', verbose_name='Closedquote' , blank=True, null=True)
 
 
 
@@ -56,7 +56,7 @@ class UnauthorisedCallBackRequests(models.Model):
     time_requested = models.DateTimeField(default=timezone.now)
     closed_by = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-        related_name='Closed_requests_Callback', verbose_name='Closed_Callback', blank=True)
+        related_name='Closed_requests_Callback', verbose_name='Closed_Callback', blank=True,null=True)
 
 
 class AuthorisedTicketRequests(models.Model):
